ea64076ddc12aaabc0d71cd1152961e52bd3ced6
sena986 <53561380+sena986@users.noreply.github.com>
2021-09-21 22:10:47 +0900

    init & nav

---
+++ src/Components/App.js
@@ -0,0 +1,11 @@
+import Nav from "Components/Nav";
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Nav.js
@@ -0,0 +1,16 @@
+function Nav() {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>TOP</li>
+          <li>ABOUT US</li>
+          <li>CONTACT</li>
+          <li>RECRUITMENT</li>
+        </ul>
+      </nav>
+    </>
+  );
+}
+
+export default Nav;
